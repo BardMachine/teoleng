f59d790708fb3929f99c28d3b098d249fa1a2f45
ninobot <paulinopose@gmail.com>
2026-04-08 14:30:23 -0300

    programa2

--- lab1/programas/programa2.py
+++ lab1/programas/programa2.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
 import re
 import sys
+from programa1 import programa1
 
 def programa2(RutaFactura):
     
-    '''
-    SU CÓDIGO
-    
-    NOTA: El formato de la fecha debe ser AAAA-MM-DD 
-    '''
-    fecha = "2026-04-01"        
-    monto = 954,25
+    texto = programa1(RutaFactura)
+    fecha = re.search(r"(\d{4})-(\d{2})-(\d{2})", texto)
+    monto = re.search(r"DÉBITO BANCARIO:(\s)*(\d)*,(\d{2})", texto, flags=re.IGNORECASE)
+    monto = re.search(r"(\d)*,(\d{2})",monto)
+
     return fecha, monto
   
 
